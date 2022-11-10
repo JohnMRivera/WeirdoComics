3f320ee825b9c70225c46e160a4fb1e00f36ad5c
Brayan-Carranza <bcarranza@hotmail.com>
2022-11-10 14:41:21 -0600

    Crear formularios de Login y Registro de Usuario

--- resources/views/singup.blade.php
+++ resources/views/singup.blade.php
@@ -1,13 +1,21 @@
-@extends('layouts.plantilla')
-
-@section('titulo_documento', 'singup')
-@section('contenido')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
 
     @if (session()->has('confirmacion'))
         {!! '<script> swal( "Todo correcto", "Usuario ' .
             session()->get('confirmacion') .
             ' ah sido registrado con éxito",  "success" ) </script> ' !!}
     @endif
+    <body>
+
     <div class="container col-md-6 mt-4 cardme cont">
         <div class="card text-center font-monospace cardme">
             <div class="card-header">
@@ -68,4 +76,6 @@
                 </div>
             </form>
      </div>
-@stop
+
+     </body>
+</html>
