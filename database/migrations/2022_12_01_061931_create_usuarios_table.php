dcbc89ee0f535aaabd2a6bf373be31c74d2b7a21
JohnMRivera <johnrivera310802@gmail.com>
2022-12-04 22:18:55 -0600

    Se crean las migraciones del CRUD

---
+++ database/migrations/2022_12_01_061931_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id('id_usuario');
+            $table->string('nombre_usuario');
+            $table->string('apellido_p');
+            $table->string('apellido_m');
+            $table->string('rol');
+            $table->string('correo');
+            $table->string('contra');
+            $table->string('telefono');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
---
+++ database/migrations/2022_12_01_062143_create_pedidos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id('id_pedido');
+            $table->unsignedBigInteger('id_proveedor');
+            $table->foreign('id_proveedor')->references('id_proveedor')->on('proveedores');
+            $table->string('tipo_surtido');
+            $table->integer('cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
