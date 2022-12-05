28d6a8cc1c3205816528931f858ac2824fd54e25
JohnMRivera <johnrivera310802@gmail.com>
2022-12-04 22:20:12 -0600

    Se agregan las rutas faltantes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ControladorVistas;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorUsuariosBD;
+use App\Http\Controllers\ControladorComicsBD;
+use App\Http\Controllers\ControladorArticulosBD;
+use App\Http\Controllers\ControladorProveedoresBD;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,34 +24,76 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(ControladorVistas::class)->group(function(){
     Route::get('menu','vistaMenu')->name('ini');
-    Route::get('proveedores','vistaProveedores')->name('pro');
+    // Route::get('proveedores','vistaProveedores')->name('pro');
     Route::get('consultaCA','vistaCA')->name('cca');
     Route::get('pedidos','vistaPedidos')->name('ped');
     Route::get('/','vistaLogin')->name('log');
-    Route::get('/registro_usuario','registroUsuario')->name('reg_usu');
-    Route::get('registro_articulos','vistaRegistroA')->name('rega');
+    Route::get('registro_usuario','registroUsuario')->name('reg_usu');
+    Route::get('registro_articulos','vistaRegistroA')->name('reg_art');
     Route::get('registro_comics','vistaRegistroC')->name('reg_com');
     // Route::get('ventas_mostrador','vistaVentasM')->name('venm');
-    Route::get('registro_Proveedores', 'vistaRegistroPro')->name('regpro');
+    Route::get('registro_proveedores', 'vistaRegistroProveedores')->name('reg_pro');
     Route::get('tienda','vistaTienda')->name('shop');
     Route::get('carrito', 'vistaCarrito')->name('shCar');
 
-    Route::get('eliminar_comic','eliminarComic')->name('eli_com');
-    Route::post('agregar_comic','agregarComic')->name('agr_com');
-
-    Route::post('agregar', 'AgregarArticulo') -> name('addArti');
     // Route::post('agregar_comic', 'AgregarComic')->name('addCom');
     // Route::post('agregar_proveedor', 'AgregarProveedor')->name('addProv');
     // // Route::get('sesion','VistaSesion')->name('ses');
     
     // Procesos de validación
-    Route::post('agregar_articulo_pedido','agregarArticuloPedido')->name('agr_art');
+    // Validaciónes Login
     Route::post('procesar_login','procesarLogin')->name('pro_log');
     Route::post('procesar_registro_usuario','procesarRegistroUsuario')->name('pro_reg_usu');
-    Route::get('eliminar_proveedor','eliminarProveedor')->name('eli_pro');
+    
+    // Validaciones Registro de Usuario
+    Route::post('agregar_comic','agregarComic')->name('agr_com');
+    Route::get('eliminar_comic','eliminarComic')->name('eli_com');
+
+    // Validaciones Registro de Articulo
+    Route::post('agregar_articulo', 'AgregarArticulo') -> name('agr_art');
     Route::get('eliminar_articulo','eliminarArticulo')->name('eli_art');
+
+    // Validaciones Agenda de Proveedores
+    Route::get('eliminar_proveedor','eliminarProveedor')->name('eli_pro');
+
+    // Validaciones de Pedidos
+    Route::post('agregar_articulo_pedido','agregarArticuloPedido')->name('agr_art_ped');
+    Route::get('eliminar_articulo_pedido','eliminarArticuloPedido');
     Route::post('generar_pedido','generarPedido')->name('gen_ped');
+
+    // Validaciones Proveedores
+    // Route::post('agregar_proveedor','agregarProveedor')->name('agr_pro');
 });
 
 // 
 // Route::post('guardarRegistro',[ControladorVistas::class,'ProcesarRegistro'])->name('Sing');
+
+// Usuarios
+Route::controller(ControladorUsuariosBD::class)->group(function(){
+    Route::get('usuario','create')->name('usuario.create');
+    Route::post('usuario/store','store')->name('usuario.store');
+});
+
+// Comics
+Route::controller(ControladorComicsBD::class)->group(function(){
+    Route::get('comic','create')->name('comic.create');
+    Route::post('comic/store','store')->name('comic.store');
+    Route::delete('comic/destroy','destroy')->name('comic.destroy');
+});
+
+// Articulos
+Route::controller(ControladorArticulosBD::class)->group(function(){
+    Route::get('articulo','create')->name('articulo.create');
+    Route::post('articulo/store','store')->name('articulo.store');
+});
+
+// Proveedores
+Route::controller(ControladorProveedoresBD::class)->group(function(){
+    Route::get('proveedor','create')->name('proveedor.create');
+    Route::post('proveedor/store','store')->name('proveedor.store');
+    Route::get('proveedor_index','index')->name('proveedor.index');
+
+    Route::delete('proveedor/{id}/destroy','destroy')->name('proveedor.destroy');
+});
+
+// Route::get('proveedor/index',[ControladorProveedoresBD::class, 'index'])->name('proveedor.index');
